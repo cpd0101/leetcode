fc41d9456f8978a6baf401ac05a2f26079322c22
缠师 <shaohui.tsh@alibaba-inc.com>
2015-07-01 23:44:51 +0800

    '13'

--- Algorithms/13.Roman to Integer.js
+++ Algorithms/13.Roman to Integer.js
@@ -48,24 +48,23 @@ var romanToInt = function(s) {
  * @return {number}
  */
 var romanToInt = function(s) {
-    var obj = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
-    };
+    var map = [];
+    map['I'] = 1;
+    map['V'] = 5;
+    map['X'] = 10;
+    map['L'] = 50;
+    map['C'] = 100;
+    map['D'] = 500;
+    map['M'] = 1000;
     var len = s.length - 1;
     var n = 0;
     for (var i = 0; i < len; i++) {
-        if (obj[s[i]] < obj[s[i + 1]]) {
-            n -= obj[s[i]];
+        if (map[s[i]] < map[s[i + 1]]) {
+            n -= map[s[i]];
         } else {
-            n += obj[s[i]];
+            n += map[s[i]];
         }
     }
-    n += obj[s[len]];
+    n += map[s[len]];
     return n;
 };
