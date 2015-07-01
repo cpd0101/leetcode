d0601fb54d256bffb1f3516e177efc6d8be54e29
baidu <baidu@baidudeMacBook-Pro.local>
2015-07-01 22:14:35 +0800

    13

--- Algorithms/13.Roman to Integer.js
+++ Algorithms/13.Roman to Integer.js
@@ -40,3 +40,32 @@ var romanToInt = function(s) {
     }
     return n;
 };
+
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var obj = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    var len = s.length - 1;
+    var n = 0;
+    for (var i = 0; i < len; i++) {
+        if (obj[s[i]] < obj[s[i + 1]]) {
+            n -= obj[s[i]];
+        } else {
+            n += obj[s[i]];
+        }
+    }
+    n += obj[s[len]];
+    return n;
+};
