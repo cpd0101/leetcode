ee7def509d2d87a7627e2184247e46ae3e813567
缠师 <shaohui.tsh@alibaba-inc.com>
2015-10-04 23:33:04 +0800

    'revise'

--- Algorithms/122.Best Time to Buy and Sell Stock II.js
+++ Algorithms/122.Best Time to Buy and Sell Stock II.js
@@ -3,21 +3,22 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    var q = [];
     var len = prices.length;
     if (len < 1) return 0;
     var ret = [0];
+    var min = 0;
     for (var i = 1; i < len; i++) {
-        ret.push(0);
-        for (var j = 0; j <= i; j++) {
-            for (var k = j + 1; k <= i; k++) {
-                if (prices[k] > prices[j]) {
-                    var sum = ret[j] + prices[k] - prices[j];
-                    if (sum > ret[k]) ret[k] = sum;
+        ret.push(ret[i - 1]);
+        if (prices[min] < prices[i]) {
+            for (var j = 0; j < i; j++) {
+                if (prices[i] > prices[j]) {
+                    var sum = ret[j] + prices[i] - prices[j];
+                    if (sum > ret[i]) ret[i] = sum;
                 }
             }
+        } else {
+            min = i;
         }
-        if (ret[i] < ret[i - 1]) ret[i] = ret[i - 1];
     }
     return ret[len - 1];
 };
