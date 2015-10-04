806a15c5ba0557286bf3d2dbf3aae3adba6d5fcd
baidu <baidu@baidudeMacBook-Pro.local>
2015-10-04 21:50:38 +0800

    revise

--- Algorithms/122.Best Time to Buy and Sell Stock II.js
+++ Algorithms/122.Best Time to Buy and Sell Stock II.js
@@ -6,7 +6,6 @@ var maxProfit = function(prices) {
     var q = [];
     var len = prices.length;
     if (len < 1) return 0;
-    if (len > 100) return 0;
     var ret = [0];
     for (var i = 1; i < len; i++) {
         ret.push(0);
