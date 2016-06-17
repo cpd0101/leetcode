32e1eb6013dcae4fcbc6078604ca3ba96f07eee7
tanshaohui <tanshaohui@baidu.com>
2016-06-17 23:29:30 +0800

    Commit

--- Algorithms/336. Palindrome Pairs.js
+++ Algorithms/336. Palindrome Pairs.js
@@ -12,35 +12,47 @@ var palindromePairs = function (words) {
         return true;
     };
 
-    var map = {};
+    var reverse = function (s) {
+        var t = '';
+        for (var i = s.length - 1; i > -1; i--) {
+            t += s[i];
+        }
+        return t;
+    };
+
+    var map = new Map();
 
     for (var i = 0; i < words.length; i++) {
-        map[words[i]] = i;
+        map.set(words[i], i);
     }
 
     var ret = [];
 
     for (var i = 0; i < words.length; i++) {
-        if (map.hasOwnProperty('') && words[i] !== '' && isPalindrome(words[i])) {
-            ret.push([i, map['']]);
-            ret.push([map[''], i]);
+        if (map.has('') && words[i] !== '' && isPalindrome(words[i])) {
+            ret.push([i, map.get('')]);
+            ret.push([map.get(''), i]);
         }
 
-        var rword = words[i].split('').reverse().join('');
-        if (map.hasOwnProperty(rword) && rword !== words[i]) {
-            ret.push([i, map[rword]]);
+        var rword = reverse(words[i]);
+        if (map.has(rword) && rword !== words[i]) {
+            ret.push([i, map.get(rword)]);
         }
 
         for (var j = 1; j < words[i].length; j++) {
             var left = words[i].slice(0, j);
             var right = words[i].slice(j);
             if (isPalindrome(left)) {
-                var rright = right.split('').reverse().join('');
-                map.hasOwnProperty(rright) && ret.push([map[rright], i]);
+                var rright = reverse(right);
+                if (map.has(rright)) {
+                    ret.push([map.get(rright), i]);
+                }
             }
             if (isPalindrome(right)) {
-                var rleft = left.split('').reverse().join('');
-                map.hasOwnProperty(rleft) && ret.push([i, map[rleft]]);
+                var rleft = reverse(left);
+                if (map.has(rleft)) {
+                    ret.push([i, map.get(rleft)]);
+                }
             }
         }
     }
