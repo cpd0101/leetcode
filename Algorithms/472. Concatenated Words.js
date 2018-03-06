971b6a28b9b70877474204c67a08c414096bb0db
shaohui.tsh <shaohui.tsh@alibaba-inc.com>
2018-03-06 10:12:43 +0800

    feat: 472

---
+++ Algorithms/472. Concatenated Words.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findAllConcatenatedWordsInADict = function (words) {
+  const wordSet = new Set(words);
+  const ans = [];
+  function search (word) {
+    if (wordSet.has(word)) {
+      return true;
+    }
+    for (let i = 1; i < word.length; i++) {
+      if (wordSet.has(word.slice(0, i)) && search(word.slice(i))) {
+        return true;
+      }
+    }
+    return false;
+  }
+  words.forEach(word => {
+    wordSet.delete(word);
+    if (search(word)) {
+      ans.push(word);
+    }
+    wordSet.add(word);
+  });
+  return ans;
+};
